7f1eb3db3634894082c871cc0d44362706732d0a
Watscho Aiwasov <w4tscho@gmail.com>
2021-07-27 21:53:06 +0400

    feat: transform type default definition

--- src/conf.ts
+++ src/conf.ts
@@ -3,3 +3,6 @@ export const STYLE_VARIABLE = 'Style';
 
 /** 忽略转换类名前缀 */
 export const IGNORE_PREFIX='';
+
+// Transform Type [camelCase, snake_case, kebab-case]
+export const TRANSFORM_TYPE = 'kebab-case'
